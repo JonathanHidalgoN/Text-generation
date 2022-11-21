7b3c18868a87c47d041f12b89c864609528207ad
Jonathan <joni7840850@gmail.com>
2022-11-21 11:01:20 -0600

    Fix reading method

--- utils/read_text.py
+++ utils/read_text.py
@@ -1,16 +1,19 @@
-import csv
 
-def read_text(filename, delimiter=','):
+def read_text(filename, delimiter=',', lines_to_return=1000):
     """
     Reads a text file and returns a list of lines.
     Args:
         filename: The name of the file to read.
         delimiter: The delimiter to use when reading the file.
+        lines_to_return: The number of lines to return.
     Returns:
         A list of lines.
     """
-    with open(filename, 'r') as f:
-        reader = csv.reader(f, delimiter=delimiter)
-        text = [row[0] for row in reader]
-    return text
+    with open(filename, "r") as f:
+        text = f.read()
+    return text.split(delimiter)[:lines_to_return]
 
+if __name__ == "__main__":
+    X = read_text("cleaned_tweets.txt","|-|",10)
+    print(X)
+    
